f191106ac8e493e1782263441630dd05325b67c1
junhyuk <junhyuk2449@naver.com>
2021-10-24 00:29:21 +0900

    21.10.24 00:22 준혁 : Admin get, 메소드 추가, ATMInfo 분리

--- README.md
+++ README.md
@@ -3,4 +3,7 @@
 자바 atm 구현 프로젝트입니다
 
 21.10.23 23:40
-준혁 : ATM, User클래스 Arraylist 형태로 선언, get,set 추가, main에서 User 더미데이터 추가 
+준혁 : ATM, User클래스 Arraylist 형태로 선언, get,set 추가, main에서 User 더미데이터 추가
+
+21.10.24 00:22
+준혁 : Admin get, 메소드 추가, ATMInfo 분리
--- java_atm_project/src/atm_project/ATM.java
+++ java_atm_project/src/atm_project/ATM.java
@@ -4,59 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ATM {
-    protected int leftManWon = 1000;            //만원권 남은 장수
-    protected int left5ManWon = 200;           //5만원권 남은 장수
-    protected int leftCheonWon = 0;            //천원권 남은 장수
-    protected int left5CheonWon = 0;            //5천원권 남은 장수
-
-    protected List<String> atmPreInfo = new ArrayList<>();        //ATM 거래 이전 정보를 저장
-
-    protected void transactionReq() {            //거래요청
-    }
-
-    protected int getLeftManWon() {
-        return leftManWon;
-    }          //ATM 전체 남은 만원권 장수 반환
-
-    protected int getLeft5ManWon() {
-        return left5ManWon;
-    }          //ATM 전체 남은 5만원권 장수 반환
-
-    protected int getLeftCheonWon() {
-        return leftCheonWon;
-    }           //ATM 전체 남은 천원권 장수 반환
-
-    protected int getLeft5ChwonWon() {
-        return left5CheonWon;
-    }           //ATM 전체 남은 5천원권 장수 반환
-
-    protected void setManWon(int leftManWon) {
-        this.leftManWon = leftManWon;
-    } //ATM 전체 남은 만원권 장수 변경
-
-    protected void set5ManWon(int left5ManWon) {
-        this.left5ManWon = left5ManWon;
-    } //ATM 전체 남은 5만원권 장수 변경
-
-    protected void setCheonWon(int leftCheonWon) {
-        this.leftCheonWon = leftCheonWon;
-    } //ATM 전체 남은 천원권 장수 변경
-
-    protected void set5CheonWon(int left5CehonWon) {
-        this.left5CheonWon = left5CheonWon;
-    } //ATM 전체 남은 5첝원권 장수 변경
-
-//    public String getAtmPreInfo() {
-//        return atmPreInfo;
-//    }
-
-    public ATM(int leftManWon, int left5ManWon, int leftCheonWon, int left5CheonWon) {
-        this.leftManWon = leftManWon;
-        this.left5ManWon = left5ManWon;
-        this.leftCheonWon = leftCheonWon;
-        this.left5CheonWon = left5CheonWon;
-    }
-
     public static void main(String[] args) {
 
         List<User> user = new ArrayList<>();
