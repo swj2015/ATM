089eaa452ab9cc99e27b04509a87449e58137f0e
swj2015 <58240903+swj2015@users.noreply.github.com>
2021-10-24 23:18:45 +0900

    Update ATM.java

--- java_atm_project/src/atm_project/ATM.java
+++ java_atm_project/src/atm_project/ATM.java
@@ -18,10 +18,13 @@ public class ATM{
         user.add(newUser2);
         User newUser3 = new User("정수현", "test3", 6631, 3, "999988887777", "666655554444", "333322221111");
         user.add(newUser3);
+        
         Admin newAdmin1 = new Admin("admin1", 1234, "관리자1");
         admin.add(newAdmin1);
+
         Admin newAdmin2 = new Admin("admin2", 5678, "관리자2");
         admin.add(newAdmin2);
+        
         Account newAcc1 = new Account("성우진", "111122223333", 1250000, 1,1111);
         acc.add(newAcc1);
         Account newAcc2 = new Account("최준혁", "123456789012", 3800000, 1, 1111);
@@ -36,31 +39,25 @@ public class ATM{
         acc.add(newAcc6);
         
         
+        Admin ad = new Admin(null, 0, null);
+        User us =new User(null, null, 0, 0, null, null, null);
         Scanner s = new Scanner(System.in);
-        
-		Admin ad = new Admin(null, 0, null);								//Admin_클래스의 메소드를 쓰기위해 선언
-        User us = new User(null, null, 0, 0, null, null, null);				//User_클래스의 메소드를 쓰기위해 선언
-        Account ac = new Account(null, null, 0, 0, 0);
-        
-        
+   
         while(true){
 		System.out.println("관리자는 1번, 고객은 2번을 입력해주세요");
 		int a = s.nextInt();
-		
-		int cheonWon=0;
-		int ohCheonWon =0;
-		int manWon =0;
-		int ohManWon =0;
-		
-		
+	
 		switch(a){
 		case 1:																			//관리자로 로그인
 				System.out.print("관리자 아이디를 입력해주세요\n");
-				String ad_id = s.next();
-				System.out.println("비밀번호를 입력해주세요\n");
-				int ad_PWD = s.nextInt();
+				String ad_id;
+				ad_id = s.next();
 				
-				ad.adminIdentification(ad_id,ad_PWD);							//관리자 로그인 성공 실패 메소드
+				System.out.println("비밀번호를 입력해주세요");
+				int ad_PWD;
+				ad_PWD = s.nextInt();
+				
+				ad.adminIdentification(ad_id,ad_PWD);									//관리자 로그인 성공 실패 메소드
 				
 				System.out.println("1을 누르면 관리자 지폐 입금을, 2을 누르면 관리자 지폐 출금, 3을 누르면 exit 합니다.\n");		
 				int n = s.nextInt();
@@ -72,12 +69,18 @@ public class ATM{
 				case 1:
 						System.out.print("관리자 지폐 입금을 시작합니다");
 						
+						int cheonWon = s.nextInt();		//입금할 지폐 장수 입력
+						int ohCheonWon = s.nextInt();
+						int manWon = s.nextInt();
+						int ohManWon = s.nextInt();
+						
 						ad.adminBillAdd(cheonWon,ohCheonWon,manWon,ohManWon);
+						
 						break;
 				case 2:	
 						System.out.print("관리자 지폐 출금을 시작합니다");
 						
-						ad.adminBillTake(cheonWon,ohCheonWon,manWon,ohManWon);
+						//ad.adminBillTake(cheonWon,ohCheonWon,manWon,ohManWon);
 						break;
 				case 3:	
 						System.exit(0);
@@ -99,22 +102,23 @@ public class ATM{
 				System.out.println("1을 누르면 입금, 2을 누르면 출금, 3을 누르면 계좌간 거래, 4를 누르면 계좌 조회, 5를 누르면 exit");	
 				
 				int a1 = s.nextInt();
-				
-				//int deposit =0;
-				//int withdraw =0;
-				
+	
 				switch(a1){
-					
+				
 				case 1:
 						System.out.print("입금을 시작합니다. 입금할 금액을 입력해주세요.\n");
-						int deposit = s.nextInt();		//입금할 금액 입력
 						
-						ac.depositReq(acc.get(0).getAccNum(), acc.get(0).getAccPWD(), acc.get(0).getAccBal(), cheonWon, ohCheonWon, manWon, ohManWon);
+						int deposit1 = s.nextInt();		//입금할 지폐 장수 입력
+						int deposit2 = s.nextInt();
+						int deposit3 = s.nextInt();
+						int deposit4 = s.nextInt();
+						
+						//ac.depositReq(acc.get(0).getAccNum(), acc.get(0).getAccPWD(), acc.get(0).getAccBal(), deposit1, deposit2, deposit3, deposit4);
 						break;
 				case 2:	
 						System.out.print("출금을 시작합니다. 출금할 금액을 입력해주세요.\n");
 						int withdraw = s.nextInt();
-						ac.withdrawReq(acc.get(0).getAccNum(), acc.get(0).getAccPWD(), acc.get(0).getAccBal(), cheonWon, ohCheonWon, manWon, ohManWon);
+						//ac.withdrawReq(acc.get(0).getAccNum(), acc.get(0).getAccPWD(), acc.get(0).getAccBal(), cheonWon, ohCheonWon, manWon, ohManWon);
 						break;
 				case 3:
 						System.out.print("계좌간 거래를 시작합니다");
@@ -122,7 +126,7 @@ public class ATM{
 						break;
 				case 4:	
 						System.out.print("계좌 조회를 시작합니다");
-						us.depositSearch(str);						//처음 입력받은 고객 아이디에 해당하는 계좌 조회
+						//us.depositSearch(str);						//처음 입력받은 고객 아이디에 해당하는 계좌 조회
 						break;
 				case 5:	
 						System.exit(0);
