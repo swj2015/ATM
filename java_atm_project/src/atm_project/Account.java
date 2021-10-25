a6d79051cd8cb1c9131b64363d011afa80c3d471
junhyuk <junhyuk2449@naver.com>
2021-10-25 17:03:40 +0900

    Account  메소드 수정

--- java_atm_project/src/atm_project/Account.java
+++ java_atm_project/src/atm_project/Account.java
@@ -15,7 +15,7 @@ public class Account {
 
 	protected int depositReq(String accNum, int accPWD, int total, int cheonWon, int ohCheonWon, int manWon, int ohManWon){ //입금요청
 		for (int i=0 ; i<acc.size(); i++){
-			if (accNum == acc.get(i).getAccNum() && accPWD == acc.get(i).getAccPWD()){
+			if (accNum.equals(acc.get(i).getAccNum()) && accPWD == acc.get(i).getAccPWD()){
 				transaction.accBalAddReq(accNum, total);
 				transaction.transLogReq(accNum, acc.get(i).getAccUser(), total, manWon, ohManWon, cheonWon, ohCheonWon, acc.get(i).getAccBal());
 				transaction.atmLeftAddReq(cheonWon, ohCheonWon, manWon, ohManWon);
@@ -30,28 +30,28 @@ public class Account {
 	
 	protected int withdrawReq(String accNum, int accPWD, int total, int cheonWon, int ohCheonWon, int manWon, int ohManWon){//출금요청
 		for (int i=0 ; i<acc.size(); i++) {
-			if (accNum == acc.get(i).getAccNum() && accPWD == acc.get(i).getAccPWD()){
-				if (acc.get(i).getAccBal() < total){
-					System.out.println ("계좌의 잔액이 부족합니다!");
+			if (accNum.equals(acc.get(i).getAccNum()) && accPWD == acc.get(i).getAccPWD()) {
+				if (acc.get(i).getAccBal() < total) {
+					System.out.println("계좌의 잔액이 부족합니다!");
 					return 2001;
 				}
-				if(atmInfo.leftCheonWon < cheonWon || atmInfo.left5CheonWon < ohCheonWon || atmInfo.leftManWon < manWon || atmInfo.left5ManWon < ohManWon){
+				if (atmInfo.leftCheonWon < cheonWon || atmInfo.left5CheonWon < ohCheonWon || atmInfo.leftManWon < manWon || atmInfo.left5ManWon < ohManWon) {
 					System.out.println("ATM기 내의 지폐가 부족합니다!");
 					return 2002;
 				}
 				transaction.accBalSubReq(accNum, total);
-				break;
+				transaction.transLogReq(accNum, acc.get(i).getAccUser(), -total, manWon, ohManWon, cheonWon, ohCheonWon, acc.get(i).getAccBal());
+				transaction.atmLeftSubReq(cheonWon, ohCheonWon, manWon, ohManWon);
+				return 1000;
 			}
-			transaction.transLogReq(accNum, acc.get(i).getAccUser(), -total, manWon, ohManWon, cheonWon, ohCheonWon, acc.get(i).getAccBal());
 		}
-		transaction.atmLeftSubReq(cheonWon, ohCheonWon, manWon, ohManWon);
-
-		return 1000;
+		System.out.println("계좌 정보가 일치하지 않습니다!");
+		return 2000;
 	}
 	
 	protected int remitReq(String sendAccNum, int sendAccPWD, String sentAccNum, int total){				//거래요청(타계좌간거래)
 		for (int i=0 ; i<acc.size(); i++) {
-			if (sendAccNum == acc.get(i).getAccNum() && sendAccPWD == acc.get(i).getAccPWD()){
+			if (sendAccNum.equals(acc.get(i).getAccNum()) && sendAccPWD == acc.get(i).getAccPWD()){
 				if (acc.get(i).getAccBal() < total){
 					System.out.println ("계좌의 잔액이 부족합니다!");
 					return 2001;
