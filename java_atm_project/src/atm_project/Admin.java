ff422cab05e61dcf0060fa4e6e2387dfda064962
swj2015 <58240903+swj2015@users.noreply.github.com>
2021-10-26 00:11:45 +0900

    Add files via upload

--- java_atm_project/src/atm_project/Admin.java
+++ java_atm_project/src/atm_project/Admin.java
@@ -8,7 +8,7 @@ public class Admin {
 	ArrayList<AdminInfo> admin;
 	ArrayList<Transaction_Log> transLog;
 	Transaction trans = new Transaction();
-	ATMInfo atmInfo = new ATMInfo();
+	ATM atm;
 
 	public Admin(ArrayList<AdminInfo> admin) {
 		this.admin = admin;
@@ -26,20 +26,24 @@ public class Admin {
 		return 2000;
 	} //1000 : 성공 2000 : 오류
 
-	protected int adminBillAdd(int cheonWon, int ohCheonWon, int manWon, int ohManWon){
-		trans.atmLeftAddReq(cheonWon, ohCheonWon, manWon, ohManWon);
-		System.out.println("성공적으로 지폐를 보충했습니다!");
-		return 1000;
-	}//관리자 지폐 입금
-
-	protected int adminBillTake(int cheonWon, int ohCheonWon, int manWon, int ohManWon){
-		if(atmInfo.leftCheonWon < cheonWon || atmInfo.left5CheonWon < ohCheonWon || atmInfo.leftManWon < manWon || atmInfo.left5ManWon < ohManWon){
-			System.out.println("ATM기 내의 지폐가 부족합니다!");
-			return 2001;
-		}
-		trans.atmLeftSubReq(cheonWon, ohCheonWon, manWon, ohManWon);
-		System.out.println("성공적으로 지폐를 꺼냈습니다!");
-		return 1000;
-	}//관리자 지폐 출금
+//	protected int adminBillAdd(int cheonWon, int ohCheonWon, int manWon, int ohManWon){
+//		atm.billAdd(cheonWon,ohCheonWon, manWon, ohManWon);
+//		System.out.println("성공적으로 지폐를 보충했습니다!");
+//		System.out.println("ATM기 안에 남은 천원권 장 수는 : " + atm.leftCheonWon);
+//		System.out.println("ATM기 안에 남은 오천원권 장 수는 : " + atm.left5CheonWon);
+//		System.out.println("ATM기 안에 남은 만원권 장 수는 : " + atm.leftManWon);
+//		System.out.println("ATM기 안에 남은 오만원권 장 수는 : " + atm.left5ManWon);
+//		return 1000;
+//	}//관리자 지폐 입금
+//
+//	protected int adminBillTake(int cheonWon, int ohCheonWon, int manWon, int ohManWon){
+//		if(atm.leftCheonWon < cheonWon || atm.left5CheonWon < ohCheonWon || atm.leftManWon < manWon || atm.left5ManWon < ohManWon){
+//			System.out.println("ATM기 내의 지폐가 부족합니다!");
+//			return 2001;
+//		}
+//		//trans.atmLeftSubReq(cheonWon, ohCheonWon, manWon, ohManWon);
+//		System.out.println("성공적으로 지폐를 꺼냈습니다!");
+//		return 1000;
+//	}//관리자 지폐 출금
 
 }
